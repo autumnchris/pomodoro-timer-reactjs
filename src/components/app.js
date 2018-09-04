3764e6ff8c6472922448f0477717d969d27c979c
Autumn <autumncbullard@gmail.com>
2018-09-03 21:53:16 -0400

    Add markup for timers and timer buttons

--- src/components/app.js
+++ src/components/app.js
@@ -16,6 +16,26 @@ export default class App extends Component {
           <h1>Pomodoro Timer</h1>
         </header>
         <main>
+          {/* TIMERS */}
+          <div className="timers">
+            <div className="timer-card">
+              <h2>Work Session</h2>
+              <div className="timer">25:00</div>
+            </div>
+            <div className="timer-card">
+              <h2>Break Session</h2>
+              <div className="timer">05:00</div>
+            </div>
+          </div>
+          {/* TIMER BUTTONS */}
+          <div className="button-group">
+            <button type="button" className="timer-button" aria-label="Play" title="Play">
+              <span className="fa fa-play fa-2x"></span>
+            </button>
+            <button type="button" className="timer-button" aria-label="Reset" title="Reset">
+              <span className="fa fa-redo-alt fa-2x"></span>
+            </button>
+          </div>
           {/* AUDIO */}
           <audio src={audio} />
         </main>

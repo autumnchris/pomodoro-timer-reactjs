9250af1f4613f5b69df38f7f2b0d7933a28af98e
autumnchris <autumncbullard@gmail.com>
2022-03-11 14:20:50 -0500

    Update form markup

--- src/components/settings-modal.js
+++ src/components/settings-modal.js
@@ -10,15 +10,15 @@ const SettingsModal = ({ workValue, breakValue, handleChange, handleSubmit, sett
           <form className="settings-form" onSubmit={(event) => handleSubmit(event, workValue, breakValue)} id="settings-form" noValidate>
             <div className="form-group">
               <label htmlFor="work-value">Work:</label>
-              <input type="text" name="workValue" onChange={(event) => handleChange(event)} value={workValue} id="work-value" />
+              <input type="text" name="workValue" onChange={(event) => handleChange(event)} value={workValue} inputMode="numeric" id="work-value" required />
             </div>
             <div className="form-group">
               <label htmlFor="break-value">Break:</label>
-              <input type="text" name="breakValue" onChange={(event) => handleChange(event)} value={breakValue} id="break-value" />
+              <input type="text" name="breakValue" onChange={(event) => handleChange(event)} value={breakValue} inputMode="numeric" id="break-value" required />
             </div>
             <div className="button-group">
-              <input type="submit" className="button modal-button" value="Save" />
-              <input type="button" className="button modal-button cancel" onClick={() => showModal(false)} value="Cancel" />
+              <button type="submit" className="button modal-button">Save</button>
+              <button type="button" className="button modal-button cancel" onClick={() => showModal(false)}>Cancel</button>
             </div>
           </form>
           {settingsFormError.isError ? <p className="message error-message"><span className="fa fa-exclamation-circle fa-lg fa-fw"></span> {settingsFormError.timer} must be a number greater than 0 and less than 61.</p> : null}
